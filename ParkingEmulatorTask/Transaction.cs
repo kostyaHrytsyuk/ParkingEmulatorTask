9a08d0b24e70718e0e81fc8f445e563abc78279f
Kostya <streetman889@gmail.com>
2018-05-10 22:07:20 +0300

    Added Transaction class with properties mentioned in ticket

---
+++ ParkingEmulatorTask/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParkingEmulatorTask
+{
+    class Transaction
+    {
+        public DateTime TransactionTime { get; set; }
+
+        public int CarId { get; set; }
+
+        public decimal WrittenOffMoney { get; set; }
+
+    }
+}
