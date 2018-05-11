a0c3179a1ba71c3c8baadb86f7d2512d4952a22c
kostyaHrytsyuk <streetman889@gmail.com>
2018-05-11 14:17:25 +0300

    Realized Singleton pattern in Parking class

--- ParkingEmulatorTask/Parking.cs
+++ ParkingEmulatorTask/Parking.cs
@@ -1,9 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ParkingEmulatorTask
 {
     class Parking
     {
+        #region Parking Creation
+
+        private static readonly Lazy<Parking> lazyInstance = new Lazy<Parking>(() => new Parking());
+
+        public static Parking Instance { get { return lazyInstance.Value; } }
+
+        private Parking() { }
+
+        #endregion
+        
         public List<Car> Cars { get; }
 
         public List<Transaction> Transactions { get; set; }
