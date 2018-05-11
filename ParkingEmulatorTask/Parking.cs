d9e9fd68feaecf92b3ad866051efabfdcd2950b5
kostyaHrytsyuk <streetman889@gmail.com>
2018-05-11 13:36:04 +0300

    Added Parking class

---
+++ ParkingEmulatorTask/Parking.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ParkingEmulatorTask
+{
+    class Parking
+    {
+        public List<Car> Cars { get; }
+
+        public List<Transaction> Transactions { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
